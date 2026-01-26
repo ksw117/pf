387bbb329888c5108a1d21e9a869585b74783bba
ksw117 <ksw11768@gmail.com>
2026-01-26 14:55:27 +0900

    js

---
+++ pf/js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.work_title').forEach((title) => {
+      title.addEventListener('click', () => {
+        title.classList.toggle('active');
+      });
+    });
+  });
